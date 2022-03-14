72d7ac5423f3a220005c5429b06738b12f9be5b6
gordun209 <ali-han80@hotmail.com>
2022-03-14 22:18:03 +0300

    handle login/logout route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,16 @@
 import Link from 'next/link'
 
 import { useAppSelector } from '../app/hooks'
+import { LogoutButton } from '../pages/LogoutButton'
 
 // Top navbar
 export default function Navbar() {
-  const user = useAppSelector(state => state.auth)
+  const user = useAppSelector(state => state.auth.user)
   return (
     <nav className='navbar'>
       <ul>
         <li>
-          <Link href='/'>
+          <Link href='/blogs'>
             <button className='btn-logo'>blogs</button>
           </Link>
         </li>
@@ -19,16 +20,17 @@ export default function Navbar() {
         {/* user is signed-in and has username */}
         {user && (
           <>
+            <LogoutButton />
             <li className='push-left'>
               <Link href='/admin'>
-                <button className='btn-blue'>Write Posts</button>
+                <button className='btn-blue'>Write Blog</button>
               </Link>
             </li>
           </>
         )}
 
         {/* user is not signed OR has not created username */}
-        {user && (
+        {!user && (
           <li>
             <Link href='/enter'>
               <button className='btn-blue'>Log in</button>
--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,62 +1,11 @@
-import React, { FormEventHandler, SyntheticEvent } from 'react'
+import React from 'react'
 
-import { useAppDispatch, useAppSelector } from '../app/hooks'
-import { loginUser, logout } from '../features/users/authSlice'
+import { useAppSelector } from '../app/hooks'
+import { LoginForm } from './LoginForm'
 
 const EnterPage = () => {
-  const dispatch = useAppDispatch()
-  const [username, setUsername] = React.useState('')
-  const [password, setPassword] = React.useState('')
   const user = useAppSelector(state => state.auth.user)
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault()
-    try {
-      dispatch(loginUser(username, password))
-      setUsername('')
-      setPassword('')
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  return (
-    <div
-      className='card'
-      style={{
-        width: '400px',
-        margin: 'auto',
-        marginTop: '100px',
-        padding: '20px'
-      }}
-    >
-      {!user ? (
-        <form onSubmit={handleSubmit}>
-          <h1>Login</h1>
-          <label>
-            Email:
-            <input
-              value={username}
-              type='text'
-              onChange={e => setUsername(e.target.value)}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              value={password}
-              type='password'
-              onChange={e => setPassword(e.target.value)}
-            />
-          </label>
-          <input type='submit' value='Login' />
-        </form>
-      ) : (
-        <div>
-          <button type='button' onClick={() => dispatch(logout())}>
-            Log out
-          </button>
-        </div>
-      )}
-    </div>
-  )
+
+  return <>{!user ? <LoginForm /> : null}</>
 }
 export default EnterPage
